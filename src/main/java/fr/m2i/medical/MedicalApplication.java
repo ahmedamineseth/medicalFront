1f8457bb59e025936e2a73ad3fc96bf06d945362
Amine <resa@booking-tunisie.com>
2021-11-24 14:36:51 +0100

    configure cors

--- src/main/java/fr/m2i/medical/MedicalApplication.java
+++ src/main/java/fr/m2i/medical/MedicalApplication.java
@@ -2,12 +2,27 @@ package fr.m2i.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MedicalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedicalApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //registry.addMapping("/**").allowedOrigins("http://localhost:4200"); //autorise l'accès depuis localhost:4200- GET autorisée
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+                // * dans allow origins : çad autoriser toutes les machines
+            }
+        };
     }
 
 }
